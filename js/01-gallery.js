098609e4d997607c0b8695377f4cd29fffa098b2
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-02-22 23:03:05 +0100

    do but haven't done first solution

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const divRef = document.querySelector(".gallery");
+console.log(divRef);
+
+function createDivCardMarkup() {
+  return divRef.insertAdjacentHTML(
+    "beforeend",
+    galleryItems
+      .map(({ preview, original, description }) => {
+        return `
+          <div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                  <img
+                      class="gallery__image"
+                      src="${preview}"
+                      data-source="${original}"
+                      alt="${description}"
+                  />
+              </a>
+          </div>
+          `;
+      })
+      .join("")
+  );
+}
+
+createDivCardMarkup();
+
+divRef.addEventListener("click", onImgClick);
+
+function onImgClick(e) {
+  console.log(e.target); // img
+  console.log(e.currentTarget); // div
+}
